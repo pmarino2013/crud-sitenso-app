29b7c5a7faae6fa70282bf9437bbb6af3a918b89
Pablo Marino <pmarino2013@gmail.com>
2022-01-02 22:58:43 -0300

    Agregar estructura del proyecto en el componmente App

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,24 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hola React</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-md-6 offset-md-3">
+          <h1>Contactos Query</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-6 offset-md-3">
+          {/* Listar contactos  */}
+
+          {/* Botones para actualizar y eliminar  */}
+        </div>
+      </div>
+      <div className="row my-3">
+        <div className="col-12 col-md-6 offset-md-3 d-grid gap-3">
+          {/* Botón Agregar contacto  */}
+        </div>
+      </div>
     </div>
   );
 };
